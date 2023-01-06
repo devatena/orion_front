a2d76d296b39dff12d582e5e300fb4b4df7e84bd
João Vitor <joao.vst003@gmail.com>
2023-01-06 18:42:15 -0300

    a

--- src/views/gestao-comercial/clients/index/IndexClient.ts
+++ src/views/gestao-comercial/clients/index/IndexClient.ts
@@ -1,9 +1,14 @@
 import { defineComponent } from 'vue';
+import { personAddOutline } from 'ionicons/icons'
 
 export default defineComponent({
     name: 'IndexClient',
     components: {
        
     },
-
+    setup() {
+        return{
+            personAddOutline
+        }
+    }
 });
