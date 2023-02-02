ede40de5ae05b1523cc589cdac259a91d03e366a
Herbet Junior <herbetjr@gmail.com>
2023-02-02 16:46:19 -0300

    dsd

--- src/apis/Api.ts
+++ src/apis/Api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create ({
-     baseURL: 'http://localhost:8000/api'
-    // baseURL: 'https://orion.atenaagencia.com/orion_back/public/api',
+   //   baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://orion.atenaagencia.com/orion_back/public/api',
  });
 
 axios.defaults.withCredentials = true;
